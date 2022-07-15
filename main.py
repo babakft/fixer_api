dc152d0ac88d39847e33b84ef6f4e832a5b06317
babak <babakft82@gmail.com>
2022-07-15 11:58:52 +0430

    send with email

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 from property import URL, API_KEY, RULES
 import requests
+import smtplib
 
 
 def api_result():
@@ -26,9 +27,24 @@ def commit_rules_save(file_name, file_input):
         keyfile.close()
 
 
+def send_email(rates):
+    sender = "babakft082@gmail.com"
+    receiver = "babakft082@gmail.com.com"
+
+    message = f"""\
+Subject: Hi Mailtrap
+To: {receiver}
+From: {sender}
+
+{rates}."""
+    with smtplib.SMTP("smtp.mailtrap.io", 2525) as server:
+        server.login("261f765550ac77", "669f7100c11602")
+        server.sendmail(sender, receiver, message)
+
+
 if __name__ == "__main__":
     result = api_result()
     json_result = json.loads(result)
     print(result)
     commit_rules_save(json_result["timestamp"], json_result["rates"])
-
+    send_email(json_result['rates'])
