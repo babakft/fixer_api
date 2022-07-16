86cb046430adf63cb0002ebc4878f08733afc682
babak <babakft82@gmail.com>
2022-07-16 11:47:57 +0430

    confi debug

--- config.py
+++ config.py
@@ -3,10 +3,19 @@ URL = "https://api.apilayer.com/fixer/latest?base=USD"
 API_KEY = "ODcn4W0uYqyyRVoY2xYCVmR5thzggjF2"
 
 RULES = {
-    "save": True,
+    "save": False,
     "email": {
-        "enable": True,
+        "enable": False,
         "receiver": 'babakft082@gmail.com',
         "preferred": ['BTC', 'IRR']
+    },
+    "notification": {
+        "enable": False,
+        "receiver": '09337715290',
+        "preferred": {
+             'BTC': {'min': 0.000101, 'max': 0.000110},
+             'IRR': {'min': 30000, 'max': 50000}
+         }
+
     }
 }
